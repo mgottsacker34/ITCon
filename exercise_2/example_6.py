dfe0a05fdb797b34d8c5a2feec5b644f9fb2d271
mgottsacker34 <mgottsacker34@gmail.com>
2017-07-13 07:59:16 -0500

    Comments in exercise_2

--- exercise_2/example_6.py
+++ exercise_2/example_6.py
@@ -2,6 +2,8 @@
 
 # process the data's genres.  When a movie falls into more than one genre, they are delimited with a |
 movie_genres = df_copy['genres'].map(lambda x:x.split('|'))
+
+# create array of the genres
 genres = []
 for genre in movie_genres:
     if(len(genre) >= 2):
@@ -14,9 +16,7 @@ for genre in movie_genres:
 genres = set(genres)
 
 
-
-
-
+# process list of genres
 def process_genre(genres):
     genre_list = []
     for genre in genres.split('|'):
@@ -26,17 +26,20 @@ df_copy['genre_list'] = df_copy['genres'].map(process_genre)
 df_copy.head()
 
 
-
-
+# process genre totals
 df_copy = df_copy.reset_index(drop=True)
 total_genre_list = []
+
 for idx in range(len(df_copy)):
     for genre in df_copy['genre_list'][idx]:
         total_genre_list.append(genre)
+
 genre_counter =Counter(total_genre_list)
+# create list of keys, and a list of values
 genre_counter_indx = np.asarray(list(genre_counter.keys()))
 genre_counter_val = np.asarray(list(genre_counter.values()))
 
+# create barchart based on genre totals
 fig,ax = plt.subplots(figsize=(8,6))
 sns.barplot(x = genre_counter_indx, y = genre_counter_val,color='#90caf9',ax=ax)
 plt.title('Total Genre')
