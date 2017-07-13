5af4739732a19e35e257e15648fd2be2ae088928
mgottsacker34 <mgottsacker34@gmail.com>
2017-07-13 11:42:59 -0500

    Added Jupyter Notebook

--- exercise_2/example_1.py
+++ exercise_2/example_1.py
@@ -12,8 +12,7 @@ from collections import Counter
 
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-from subprocess import check_output
-print(check_output(["ls", "../input"]).decode("utf8"))
+
 
 #----------STOP AND PRESS SHIFT + ENTER----------
 
